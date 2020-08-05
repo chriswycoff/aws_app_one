ec7aaf4e6ffe713debd20afe3ef8c762a34ffb76
chriswycoff <wycoffc@air-weigh.com>
2020-08-05 16:42:51 -0700

    added dynamodb notes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_zHBKwV2gV",
     "aws_user_pools_web_client_id": "2e1mtgbdl7n9m32egm3iqslpr",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://xcpvmlifcnepfezvrek6mq7iqa.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-44qnbffmpvbypixz4zni7oemvi"
 };
 
 
