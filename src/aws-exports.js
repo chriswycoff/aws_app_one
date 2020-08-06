23b73426dae173ce65e7be96f521fb036b1f7cef
chriswycoff <wycoffc@air-weigh.com>
2020-08-06 09:09:32 -0700

    added images

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://xcpvmlifcnepfezvrek6mq7iqa.appsync-api.us-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-44qnbffmpvbypixz4zni7oemvi"
+    "aws_appsync_apiKey": "da2-44qnbffmpvbypixz4zni7oemvi",
+    "aws_user_files_s3_bucket": "cwimages153118-dev",
+    "aws_user_files_s3_bucket_region": "us-west-2"
 };
 
 
